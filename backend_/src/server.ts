d99f511941fe048ededef547bc6efe36f1975721
Fernando <fernandopereiraaraujo_01@hotmail.com>
2026-01-26 15:26:45 -0300

    feat:criando dashbord inicial

--- backend_/src/server.ts
+++ backend_/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { routes } from './routes/routes';
 import { initAssociations } from './models/associations';
+import cors from 'cors';
 
 const app = express();
 initAssociations();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use(routes);
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
