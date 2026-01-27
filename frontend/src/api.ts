01d377d2211edc31cb9587a04ca7d434454acadb
Fernando <fernandopereiraaraujo_01@hotmail.com>
2026-01-27 10:51:43 -0300

    fix: usar variavel de ambiente na api

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -5,7 +5,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333', 
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333',
     headers: {
       Authorization: `Bearer ${cookies['@nextauth.token']}`
     }
